3a91d55be367a3157b2d432d670e6bd313c3c304
candeortiz <candelaria@marias-mbp>
2016-06-01 18:00:53 +0200

    added coments

--- src/number/ReverseNumber.java
+++ src/number/ReverseNumber.java
@@ -6,21 +6,25 @@ public class ReverseNumber {
 
 	public static void main(String[] args) {
 		System.out.println("Please enter a number: ");
+		// Read number from keyboard
 		Scanner keyboard = new Scanner(System.in);
 		int x = keyboard.nextInt();
 		String s = "";
+		// convert to char array
 		String num = Integer.toString(x);
 		char[] y = num.toCharArray();
 
 		System.out.println("Your number: " + x);
 
 		if (x < 0) {
+			// Handle negative numbers
 			s = "-";
 			for (int i = (y.length - 1); i >= 1; i--) {
 
 				s = s + y[i];
 			}
 		} else {
+			// Handle positive numbers
 			for (int i = (y.length - 1); i >= 0; i--) {
 
 				s = s + y[i];
